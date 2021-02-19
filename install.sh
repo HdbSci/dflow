142210f73cc9f33dc811ed5016d3c10b1629cbdb
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2021-02-19 20:29:28 +0000

    Fix errors in installer shell script (install.sh)

--- install.sh
+++ install.sh
@@ -7,28 +7,11 @@
 # Visit https://github.com/HdbSci/dflow
 
 
-get_repo() {
-	git clone https://github.com/HdbSci/dflow
-}
-
-build() {
-	cd dflow && python3 setup.py sdist bdist_wheel
-}
-
-install() {
-	pip3 install dflow/dist/dflow-1.0.0-py3-none-any.whl
-}
-
-
-
 #==== main ====#
 echo "Getting source code from GitHub..."
-get_repo > /dev/null
-
-echo "Building dflow wheel file..."
-build > /dev/null
+git clone https://github.com/HdbSci/dflow.git > /dev/null
 
 echo "Installing dflow..."
-install > /dev/null
+python3 dflow/setup.py install > /dev/null
 
 echo "Done!"
