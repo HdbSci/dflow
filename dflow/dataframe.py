da8355dde05393b579bbf822f5ad870c704704c7
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2021-02-21 12:43:29 +0000

    Create dataframe module with dfdataframe class (dflow/dataframe.py)

---
+++ dflow/dataframe.py
@@ -0,0 +1,19 @@
+# DFLOW LIBRARY:
+#  dflow dataframes module
+# with dfdataframe class and
+# asCSV function
+
+from dflow import utils
+
+
+# dfdataframe #
+class dfdataframe:
+    def __init__(self, rows=['hello'], columns=['a'], data=[[1]]):
+        self.rows={}
+        self.columns=utils.dictenumerate(columns)
+
+        for r, d in zip(rows, data):
+            self.rows[r] = d
+
+    def at(self, row, column):
+        return self.rows[row][self.columns[column]]
