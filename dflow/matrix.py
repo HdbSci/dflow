2a4c85c7b2a5ac117b2ad6fbc714aaa2ba4146a7
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2021-02-21 17:19:13 +0000

    Create matrix module with dfmatrix class (dflow/matrix.py)

---
+++ dflow/matrix.py
@@ -0,0 +1,43 @@
+# DFLOW LIBRARY:
+#  dflow matrices module
+# with dfmatrix class
+
+from dflow.array import dfarray
+
+
+# dfmatrix #
+class dfmatrix:
+    def __init__(self, mat):
+        self.value = dfarray([])
+        for m in mat:
+            self.value << dfarray(m)
+
+    def __matmul__(self, other):
+        x = self.list
+        y = self.list
+        # TODO: Write matrix multiplication
+
+    def row(self, index):
+        return self.value.at(index)
+
+    @property
+    def list(self):
+        res = []
+        for l in self.value.iter:
+            res.append(l.list)
+        return res
+
+    @property
+    def iter(self):
+        res = []
+        for l in self.value.iter:
+            res.append(l.list)
+        return res
+
+    @property
+    def copy(self):
+        return dfmatrix(self.list.copy())
+
+    def aply(self, f=lambda x: x):
+        for r in self.value.iter:
+            r.aply(f)
