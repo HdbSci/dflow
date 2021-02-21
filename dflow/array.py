9dda296918793d1231f93e8711a0b27ecc1e50b1
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2021-02-21 17:25:54 +0000

    Add zeros and ones functions to array module (dflow/array.py)

--- dflow/array.py
+++ dflow/array.py
@@ -139,3 +139,17 @@ def div(x, y):   # divide elements of 2 dfarray
 # linspace #
 def linspace(_from, _to, _nums):
     return dfarray(range(_from, _to, _nums / (_to - _from)))
+
+
+# zeros & ones #
+def zeros(num):
+    res = dfarray([])
+    for i in range(0, num):
+        res << 0
+        return res
+
+def ones(num):
+    res = dfarray([])
+    for i in range(0, num):
+        res << 1
+    return res
