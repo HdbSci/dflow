7a47357c517606abcd22e44f49093855a9aaf741
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2021-03-03 20:00:44 +0000

    Add sin and cos to math module (dflow/math.py)

--- dflow/math.py
+++ dflow/math.py
@@ -15,6 +15,12 @@ e = 2.7182818284
 
 
 # functions #
+# Visit https://en.wikipedia.org/wiki/Taylor_series for
+# more information about Taylor series and aproximations
+# of sin, cos, tan, exp, log...
+sin = lambda x: sum([((-1)**n)/(fact(2*n+1))*(x**(2*n+1)) for n in range(100)])
+cos = lambda x: sum([((-1)**n)/(fact(2*n))*(x**(2*n)) for n in range(100)])
+
 sinh = lambda x: e**x - e**-x
 cosh = lambda x: e**x + e**-x
 
