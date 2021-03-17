06dff37cd318cc28173618a90210e01cfabef0d3
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2021-03-17 20:42:21 +0000

    Create matrices module with zeros and ones functions (dflow/matrices.py)

---
+++ dflow/matrices.py
@@ -0,0 +1,11 @@
+# DFLOW LIBRARY:
+#  dflow matrices module
+# with zeros and ones
+# functions
+
+def zeros(rows, columns):
+    return [[0]*columns]*rows
+
+
+def ones(rows, columns):
+    return [[1]*columns]*rows
